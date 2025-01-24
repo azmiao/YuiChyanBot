f0c3e73ab167b79565e50463ebb882bce32d3052
azmiao <2362020227@qq.com>
2025-01-24 17:19:34 +0800

    修复广播错误

--- yuiChyan/core/manager/broadcast.py
+++ yuiChyan/core/manager/broadcast.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from yuiChyan import get_bot, logger, YuiChyan
+from yuiChyan import logger, YuiChyan
 from yuiChyan.exception import *
 from .util import sv
 
@@ -14,36 +14,34 @@ bc_example = f'''
 '''.strip()
 
 
-def parse_command(ev, command_raw: str):
+def parse_command(ev: CQEvent, command_raw: str):
     if ' ' not in command_raw:
         return 'all', command_raw
     args = command_raw.split(' ', 1)
-    bc_sv_name = args[0]
+    bc_group = args[0]
     bc_msg = args[1]
-    if bc_sv_name != 'all' and not bc_sv_name.startswith('g-') and not bc_sv_name.startswith('exg-'):
+    if bc_group != 'all' and not bc_group.startswith('g-') and not bc_group.startswith('exg-'):
         raise CommandErrorException(ev, f'> 广播命令错误，{bc_example}')
-    return bc_sv_name, bc_msg
+    return bc_group, bc_msg
 
 
 # 广播消息
-@sv.on_command('广播', force_private=True)
+@sv.on_command(('广播', 'bc'), force_private=True)
 async def broadcast(bot: YuiChyan, ev: CQEvent):
     command_raw = str(ev.message).strip()
-    bc_sv_name, bc_msg = parse_command(ev, command_raw)
-    yui_bot = get_bot()
-    self_id_list = yui_bot.get_self_ids()
-    for self_id in self_id_list:
+    bc_group, bc_msg = parse_command(ev, command_raw)
+    for self_id in bot.get_self_ids():
         match self_id:
             case 'all':
                 _group_id_list = await bot.get_group_list(self_id=self_id)
                 group_id_list = [int(group['group_id']) for group in _group_id_list]
-            case x if x.startswith('g-'):
-                bc_l = bc_sv_name.replace('g-', '')
+            case bc_group if bc_group.startswith('g-'):
+                bc_l = bc_group.replace('g-', '')
                 group_id_list = [int(x) for x in bc_l.split(r'/')]
-            case x if x.startswith('exg-'):
+            case bc_group if bc_group.startswith('exg-'):
                 _group_id_list = await bot.get_group_list(self_id=self_id)
                 group_id_list = [int(group['group_id']) for group in _group_id_list]
-                bc_l = bc_sv_name.replace('exg-', '')
+                bc_l = bc_group.replace('exg-', '')
                 exclude_gl = [int(x) for x in bc_l.split(r'/')]
                 for ex_gid in exclude_gl:
                     group_id_list.remove(ex_gid)
@@ -51,7 +49,7 @@ async def broadcast(bot: YuiChyan, ev: CQEvent):
                 group_id_list = []
         for group_id in group_id_list:
             try:
-                await yui_bot.send_group_msg(self_id=self_id, group_id=group_id, message=bc_msg)
+                await bot.send_group_msg(self_id=self_id, group_id=group_id, message=bc_msg)
                 logger.info(f'> 广播 [{self_id}, {group_id}, {bc_msg}] 发送成功')
                 await asyncio.sleep(1)
             except Exception as e:
